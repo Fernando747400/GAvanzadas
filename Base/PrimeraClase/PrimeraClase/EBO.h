48dad58f4b164278e0cb637fe165d837801c65ee
Morado2Amerike <Morado2Amerike@amerike.edu.mx>
2023-02-27 17:54:48 -0600

    EBO abstract

--- Base/PrimeraClase/PrimeraClase/EBO.h
+++ Base/PrimeraClase/PrimeraClase/EBO.h
@@ -1,1 +1,20 @@
-#pragma once
+#ifndef EBO_CLASS_H
+#define EBO_CLASS_H
+
+#include<glad/glad.h>
+
+class EBO
+{
+public:
+	GLuint ID;
+
+	//Constuctor to generate an Elements Buffer Object and links them to the indexes.
+	EBO(GLuint* indices, GLsizeiptr size);
+
+	void Bind();
+	void Unbind();
+	void Delete();
+};
+#endif // !EBO_CLASS_H
+
+
