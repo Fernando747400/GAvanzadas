5cc5104f19ac61dc8051bc5cd33ab185e708541a
Morado2Amerike <Morado2Amerike@amerike.edu.mx>
2023-02-27 18:44:30 -0600

    Vert class

--- Base/PrimeraClase/PrimeraClase/Main.cpp
+++ Base/PrimeraClase/PrimeraClase/Main.cpp
@@ -2,20 +2,6 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos; \n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
-const char* insideVertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos; \n"
-"void main()\n"
-"{\n"
-"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-
 int main()
 {
     glfwInit();
