3c6af29e08a5390184f77f92a11d686582c513a0
Morado2Amerike <Morado2Amerike@amerike.edu.mx>
2023-02-27 18:09:55 -0600

    VBO Header

--- Base/PrimeraClase/PrimeraClase/VBO.h
+++ Base/PrimeraClase/PrimeraClase/VBO.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef VBO_CLASS_H
+#define VBO_CLASS_H
+
+#include<glad/glad.h>
+
+class VBO
+{
+public:
+	GLuint ID;
+
+	//Constuctor to generate an Elements Buffer Object and links them to the indexes.
+	VBO(GLuint* indices, GLsizeiptr size);
+
+	void Bind();
+	void Unbind();
+	void Delete();
+};
+#endif // !VBO_CLASS_H
